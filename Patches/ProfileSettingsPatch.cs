0eb4cb13ba09f4c2822dd89d131c2f2bd962237f
Kordian Kisieliński <dczip89@gmail.com>
2023-12-10 21:31:18 +0100

    Add better settings ui

--- Patches/ProfileSettingsPatch.cs
+++ Patches/ProfileSettingsPatch.cs
@@ -4,7 +4,9 @@ using System.Reflection;
 using HarmonyLib;
 using PeterHan.PLib.Core;
 using PeterHan.PLib.UI;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TemperatureThresholds.Patches
 {
@@ -12,15 +14,36 @@ namespace TemperatureThresholds.Patches
 	public class ProfileSettingsPatch
 	{
 		// Settings Screen
-		[HarmonyPatch(typeof(UnitConfigurationScreen))]
-		[HarmonyPatch("Init")]
+		[HarmonyPatch(typeof(GameOptionsScreen))]
+		[HarmonyPatch("OnSpawn")]
 		public static class AddProfilesButton
 		{
-			static AccessTools.FieldRef<UnitConfigurationScreen, GameObject> toggleGroupRef =
-				AccessTools.FieldRefAccess<UnitConfigurationScreen, GameObject>("toggleGroup");
+			//static AccessTools.FieldRef<UnitConfigurationScreen, GameObject> toggleGroupRef =
+			//	AccessTools.FieldRefAccess<UnitConfigurationScreen, GameObject>("toggleGroup");
 
-			public static void Postfix(UnitConfigurationScreen __instance)
+			public static void Prefix(GameOptionsScreen __instance)
 			{
+				var content = __instance.GetComponent<Transform>().Find("Content");
+				if (content == null)
+				{
+					PUtil.LogWarning("Failed to find Content transform!");
+					return;
+				}
+				var saveOptions = content.Find("SaveOptions");
+				if (saveOptions == null)
+				{
+					PUtil.LogWarning("Failed to find Content/SaveOptions transform!");
+					return;
+				}
+				var saveOptionsBackground = saveOptions.Find("BG");
+				if (saveOptionsBackground == null)
+				{
+					PUtil.LogWarning("Failed to find Content/SaveOptions/BG transform!");
+					return;
+				}
+
+				var sprite = saveOptionsBackground.gameObject.GetComponent<Image>().sprite;
+				
 				PUtil.LogDebug("Injecting button into settings.");
 				var btn = new PButton
 				{
@@ -33,9 +56,109 @@ namespace TemperatureThresholds.Patches
 					ToolTip = "Opens TemperatureThresholds config directory"
 				};
 				btn.SetKleiBlueStyle();
-				GameObject go = btn.AddTo(toggleGroupRef(__instance));
+
+				var labelStyle = ScriptableObject.CreateInstance<TextStyleSetting>();
+				labelStyle.textColor = PUITuning.Fonts.UILightStyle.textColor;
+				labelStyle.style = PUITuning.Fonts.UILightStyle.style;
+				labelStyle.sdfFont = PUITuning.Fonts.UILightStyle.sdfFont;
+				labelStyle.enableWordWrapping = true;
+				labelStyle.fontSize = 24;
+				
+				PLabel label = new PLabel("TemperatureThresholdsLabel")
+				{
+					Text = "Overlay",
+					TextStyle = labelStyle,
+					TextAlignment = TextAnchor.LowerLeft,
+				};
+				var labelTransform = label.Build().GetComponent<RectTransform>();
+				
+				PPanel labelPanel = new PPanel("TemperatureThresholdsLabelPanel")
+				{
+					Direction = PanelDirection.Horizontal,
+					Spacing = 0,
+					Alignment = TextAnchor.LowerLeft
+				};
+				labelPanel.AddChild(label);
+				
+				PGridPanel panel = new PGridPanel("TemperatureThresholdsPanel")
+				{
+					Margin = new RectOffset(12, 12, 12, 12),
+					FlexSize = Vector2.left,
+					BackColor = new Color(0.188f, 0.204f, 0.263f, 1.0f),
+					BackImage = sprite,
+					ImageMode = Image.Type.Sliced,
+				};
+				panel.AddRow(new GridRowSpec());
+				panel.AddRow(new GridRowSpec());
+				panel.AddColumn(new GridColumnSpec());
+				panel.AddChild(labelPanel, new GridComponentSpec(0, 0));
+				panel.AddChild(btn, new GridComponentSpec(1, 0));
+				
+				GameObject go = panel.AddTo(content.gameObject);
+				go.transform.SetSiblingIndex(2);
 				go.SetActive(true);
+				
+				printTreeOfChildren(content);
+				printTreeOfChildren(go.transform);
 			}
+		}
+		
+		public static Transform printTreeOfChildren(Transform transform, int depth = 0)
+		{
+			string indent = "";
+			for (int i = 0; i < depth; i++)
+			{
+				indent += "  ";
+			}
+			PUtil.LogDebug(indent + transform.gameObject.name);
+			
+			// Get component names of SaveOptions and values
+			Component[] components = transform.gameObject.GetComponents<Component>();
+			foreach (Component component in components)
+			{
+				string componentName = component.GetType().Name;
+				PUtil.LogDebug(indent + "& " + componentName);
+			
+				FieldInfo[] fields = component.GetType().GetFields();
+				PropertyInfo[] properties = component.GetType().GetProperties();
+
+				foreach (FieldInfo field in fields)
+				{
+					object value = field.GetValue(component);
+					PUtil.LogDebug(indent + "&F> " + "Name: " + field.Name + ", Value: " + value);
+				}
+
+				try
+				{
+					foreach (PropertyInfo property in properties)
+					{
+						/*if (property.Name == "sprite")
+						{
+							Sprite sprite = property.GetValue(component) as Sprite;
+							var texture = sprite.texture;
+							PUtil.LogDebug(indent + "! Texture: \n" + texture.name + "\n" + texture.width + "x" + texture.height);
+							break;
+						}*/
+						try
+						{
+							object value = property.GetValue(component);
+							PUtil.LogDebug(indent + "&P> " + "Name: " + property.Name + ", Value: " + value);
+						} catch (System.Exception e)
+						{
+							PUtil.LogDebug(indent + "&P1> " + "Exception: " + e.Message);
+						}
+					}
+				} catch (System.Exception e)
+				{
+					PUtil.LogDebug(indent + "&P0> " + "Exception: " + e.Message);
+				}
+			}
+			
+			foreach (Transform child in transform)
+			{
+				printTreeOfChildren(child, depth + 1);
+			}
+			return transform;
 		}
 	}
 }
